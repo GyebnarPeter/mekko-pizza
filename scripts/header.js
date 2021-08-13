ee7711bdc3f268507221a0e1a928b14b4d4a8c7a
GyebnarPeter <gyebnarpetter@gmail.com>
2021-08-13 14:47:46 +0200

    nav scroll position

--- scripts/header.js
+++ scripts/header.js
@@ -1,4 +1,4 @@
-$('header').load('../includes/header.html', () => {
+/* $('header').load('../includes/header.html', () => {
 
     $('.header__mobile__nav-open-btn').on('click', function() {
         $('nav').css('right', '0');
@@ -7,4 +7,17 @@ $('header').load('../includes/header.html', () => {
         $('nav').css('right', '-75%');
     });
 
-});
+    let nav = document.querySelector('nav');
+    window.onscroll = () => {
+        if(window.innerWidth >= 1024) {
+            if(document.documentElement.scrollTop >= nav.offsetTop) {
+                nav.style.position = 'fixed';
+                nav.style.top = '0';
+                nav.style.left = '0';
+                nav.style.background = 'white';
+                nav.style.padding = '20px 0';
+            }
+        }
+    }
+
+}); */
---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+$('header').load('../includes/header.html', () => {
+    $('.nav-open-btn').click( () => $('nav').css('right', '0') );
+    $('.nav-close-btn').click( () => $('nav').css('right', '-75%') );
+});
+
+$(document).ready( () => {
+    let nav = document.querySelector('nav');
+
+    window.onscroll = () => {
+        if((window.innerWidth >= 1024) && (window.scrollY <= nav.offsetTop)) {
+            nav.classList.remove('nav-scroll-position');
+        } else {
+            nav.classList.add('nav-scroll-position');
+        }
+    }
+});
